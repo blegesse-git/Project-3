273f08847e894e28059c2367039bc58089115470
Beth Legesse <legessebl@gmail.com>
2020-12-06 14:03:48 -0600

    componets and events tab

---
+++ src/components/Card/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500, // this is how you can adjust the size of the card
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimpleCard(props) {
+  const classes = useStyles();
+  
+
+  return (
+  <Card className={classes.root} variant="outlined" > {props.children}</Card>
+  );
+}
