1aa96d105804bfd7824f01710574eb8f1a68addb
Luna <dlramos194@gmail.com>
2020-12-07 15:00:55 -0600

    ignore this

---
+++ src/components/Countdown/EventCountdown.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from "../Container/index"
+import Countdown from 'react-countdown';
+// import { render } from '@testing-library/react';
+
+
+function EventCountdown(){
+return(
+    <Container>
+        <h1>Countdown will go here. </h1>
+    </Container>
+)
+}
+
+
+export default EventCountdown;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,16 @@ import ToDoList from "../components/List/ToDoList"
 import Container from "../components/Container/index"
 import SetReminder from "../components/SetReminder/SetReminder"
 import OutlinedTimeline from "../components/Time line/Timeline"
+import EventTimeline from "../components/Countdown/EventCountdown"
 function Home(){
 
     return(
         <>
         <Container>
-        <ToDoList/>
+        {/* <ToDoList/> */}
         <SetReminder/>
         <OutlinedTimeline/>
+        <EventTimeline/>
     
         </Container>
       
