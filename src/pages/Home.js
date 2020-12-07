169cd004fbcf08a7ca90925d426c152db64f65a8
Luna <dlramos194@gmail.com>
2020-12-07 14:10:17 -0600

    timeline component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ToDoList from "../components/List/ToDoList"
-
+import Container from "../components/Container/index"
+import SetReminder from "../components/SetReminder/SetReminder"
+import OutlinedTimeline from "../components/Time line/Timeline"
 function Home(){
 
     return(
         <>
+        <Container>
         <ToDoList/>
+        <SetReminder/>
+        <OutlinedTimeline/>
+    
+        </Container>
+      
         </>
     )
 
