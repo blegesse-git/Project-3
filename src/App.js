b5b3fdd3ba7880c2495ca759b8cad3126d95e01e
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-12-08 19:40:52 -0600

    testing date/time

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Events from './pages/MyEvents'
 import Home from './pages/Home'
 // import GetInspired from "./pages/GetInspired"
 import Invoice from "./pages/invoice"
-// import RSVP from "./pages/RSVP"
+import RSVP from "./pages/rsvp"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
@@ -19,7 +19,7 @@ function App() {
              <Route exact path ="/" component={Home}/>
              <Route exact path ="/home" component={Home}/>
              <Route exact path ="/invoice" component={Invoice}/>
-             {/* <Route exact path ="/rsvp" component={RSVP}/> */}
+             <Route exact path ="/rsvp" component={RSVP}/>
              <Route exact path ="/events" component={Events}/>
              {/* <Route exact path ="/inspo" component={GetInspired}/> */}
         
