8b6a3d0e8a4de157e5a9b165f31a7b2fe06fd562
Beth Legesse <legessebl@gmail.com>
2020-12-15 15:26:04 -0600

    clean- up

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,12 +7,7 @@ export default {
         console.log(data);
         return axios.post("api/events", data);
     },
-    lookEvent: function(){
-        //console.log("this is util API page");
-        // console.log(data);
-        return axios.get("api/events").then(result => result.data);
-        
-    },
+    
 
     invoiceForm: function(data){
         console.log("this is util API page");
