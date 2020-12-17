661c311649fa44d856f087e2d5bd5f6edb04f20b
Luna <dlramos194@gmail.com>
2020-12-16 22:46:44 -0600

    removed reminders from home tab

--- client/src/components/ReminderForm/ReminderForm.js
+++ client/src/components/ReminderForm/ReminderForm.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Container from "@material-ui/core/Container"
-import FormControl from "@material-ui/core/FormControl"
-import { Button } from '@material-ui/core';
+// import React from 'react';
+// import { useState } from 'react';
+// import TextField from '@material-ui/core/TextField';
+// import Container from "@material-ui/core/Container"
+// import FormControl from "@material-ui/core/FormControl"
+// import { Button } from '@material-ui/core';
 
-const ReminderForm = ({ addReminder }) => {
-  const [text, setText] = useState("")
-  const handleSubmitReminder = (e) => {
-    e.preventDefault();
-    addReminder(text);
-    setText("")
-    console.log("New Reminder Alert!")
-    console.log(text)
-  }
+// const ReminderForm = ({ addReminder }) => {
+//   const [text, setText] = useState("")
+//   const handleSubmitReminder = (e) => {
+//     e.preventDefault();
+//     addReminder(text);
+//     setText("")
+//     console.log("New Reminder Alert!")
+//     console.log(text)
+//   }
 
-  return (
-    <div>
-      <Container >
-        <form onSubmit={handleSubmitReminder}>
+//   return (
+//     <div>
+//       <Container >
+//         <form onSubmit={handleSubmitReminder}>
       
-          <FormControl >
+//           <FormControl >
 
-            <TextField 
-              label="Add reminder" 
-              required={true} 
-              style={{ width: 250 }} 
-              value={text} 
-              onChange={(e) => setText(e.target.value)} />
+//             <TextField 
+//               label="Add reminder" 
+//               required={true} 
+//               style={{ width: 250 }} 
+//               value={text} 
+//               onChange={(e) => setText(e.target.value)} />
             
-            <Button 
-              variant="contained" 
-              color="primary" 
-              type="submit" 
-              style={{ width: 250 }}>Add</Button>
+//             <Button 
+//               variant="contained" 
+//               color="primary" 
+//               type="submit" 
+//               style={{ width: 250 }}>Add</Button>
 
 
-          </FormControl>
-        </form>
+//           </FormControl>
+//         </form>
 
 
-      </Container>
+//       </Container>
 
-    </div>
-  );
-}
-export default ReminderForm;
+//     </div>
+//   );
+// }
+// export default ReminderForm;
--- client/src/components/Reminders/RemindersList.js
+++ client/src/components/Reminders/RemindersList.js
@@ -1,60 +1,60 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { Container } from '@material-ui/core';
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+// import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Avatar from '@material-ui/core/Avatar';
+// import IconButton from '@material-ui/core/IconButton';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import { Container } from '@material-ui/core';
+// import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
 
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 2,
-    Width: 650,
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-  }
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 2,
+//     Width: 650,
+//   },
+//   demo: {
+//     backgroundColor: theme.palette.background.paper,
+//   }
 
-}));
-
-
+// }));
 
 
 
-const RemindersList =({title}) =>{
-  const classes = useStyles();
 
-  return (
-      <Container>
-    <div className={classes.root}>
+
+// const RemindersList =({title}) =>{
+//   const classes = useStyles();
+
+//   return (
+//       <Container>
+//     <div className={classes.root}>
    
-            <List>
-                <ListItem > 
-                  <ListItemAvatar>
-                  <Avatar> 
-                   <NotificationsActiveIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary ={title}/>
-                  <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>,  
-            </List>
-          </div>
-    </Container>
-  );
-}
+//             <List>
+//                 <ListItem > 
+//                   <ListItemAvatar>
+//                   <Avatar> 
+//                    <NotificationsActiveIcon />
+//                     </Avatar>
+//                   </ListItemAvatar>
+//                   <ListItemText primary ={title}/>
+//                   <ListItemSecondaryAction>
+//                   <IconButton edge="end" aria-label="delete">
+//                       <DeleteIcon />
+//                     </IconButton>
+//                   </ListItemSecondaryAction>
+//                 </ListItem>,  
+//             </List>
+//           </div>
+//     </Container>
+//   );
+// }
 
 
 
-export default RemindersList;
+// export default RemindersList;
