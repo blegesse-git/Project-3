7151f4d22affe1a6d26a9e32e13da47711c9a0f6
Sahiba Miglani <mg.sahiba@gmail.com>
2020-12-19 21:32:28 -0600

    changes to invoice

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
   sendgrid: {
     from: {
-      name: process.env.SENDGRID_FROM_NAME || 'Test sender',
-      email: process.env.SENDGRID_FROM_EMAIL || 'jacob.ramirez@inallmedia.com',
+      name: process.env.SENDGRID_FROM_NAME || 'Sahiba Miglani',
+      email: process.env.SENDGRID_FROM_EMAIL || 'mg.sahiba@gmail.com',
     },
     templateId_confirmation: process.env.SENDGRID_CONFIRMATION_TEMPLATE_ID || 'd-2f79cff8ac0a4697a479e24522adb10f',
-    templateId_invoice: process.env.SENDGRID_INVOICE_TEMPLATE_ID || 'd-4c8b24c9517d4e0bad1e3a56c4ae07c7',
-    apiKey: process.env.SENDGRID_API_KEY || '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',
+    templateId_invoice: process.env.SENDGRID_INVOICE_TEMPLATE_ID || 'd-2f79cff8ac0a4697a479e24522adb10f',
+    apiKey: process.env.SENDGRID_API_KEY || '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',
   },
   mongo: {
     server: process.env.MONGO_SERVER || '',
     database: process.env.MONGO_DATABASE || '',
   },
-  host: process.env.HOST || 'https://serene-citadel-52952.herokuapp.com/',
+  host: process.env.HOST || 'https://powerful-fortress-17827.herokuapp.com/',
 };
 
 module.exports = CONFIG;
+
