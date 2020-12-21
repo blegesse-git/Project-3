b8ad6659b02145ea3d2dc93e557064293f2fd75b
Luna <dlramos194@gmail.com>
2020-12-20 19:51:31 -0600

    countdown added api

--- client/src/components/Countdown/EventCountdown.js
+++ client/src/components/Countdown/EventCountdown.js
@@ -1,25 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import Container from "../Container/index"
-// import Countdown from 'react-countdown';
 import Countdown, { zeroPad, calcTimeDelta, formatTimeDelta } from 'react-countdown';
 import { useEventHook } from '../../hooks/Hooks';
+import { now } from 'mongoose';
 
-
+import API from "../../utils/API"
 function EventCountdown(props) {
 
+
+  // console.log(props.eventState)
   const eventHook = useEventHook();
+  // useEffect(() => {
+  //   API.getAllEvents().then((data) => {
+      // console.log(data);
+      // var i 
+      // for (i=0; i<data.data ; i++ ) {
+      //   console.log(data.data[i].eventDate)
+      // }
+      // console.log(data.data[0].eventDate);
+      // eventHook.replaceAllEvents(data.data);
+    // })
+    // console.log(data.data.eventDate)
+  // },[])
+  API.getAllEvents().then((data) => {
+    // console.log(data.data.length);
+    let datesArray = []
+    for (let i=0; i<data.data.length ; i++ ) {
+      // console.log(data.data[i].eventDate)
+      const eventDates = [data.data[i].eventDate]
+      // console.log(eventDates)
+      const sortedDates = eventDates.sort((a, b) => a - b)
+      // console.log(sortedDates)
+      for (let j=0; j< eventDates.length; j++){
+        // console.log( eventDates[j])
+        const dateArrays= datesArray.push(eventDates)
+        // console.log(datesArray[0])
+      }
+     
+      console.log(datesArray)
+
+    }
+    // console.log(data.data[0].eventDate);
+    // eventHook.replaceAllEvents(data.data);
+  })
+
+  // sortAscending = () => {
+    // console.log(data.data.eventDate)
+
+  
+//   const [timer, setTimer] = useState("")
+ 
+//   const [newTimer, setNewTimer] = useState([
+// {
+//   id: 1,
+//   date: "Dec 23, 2020"
+// }
+//   ]);
+  // const [timers, setTimers] = useState(props.eventState|| []);
+  // con
 
   // Random component
-  const Completionist = () => <span>Today is the day!</span>;
-  // const startCountdown = newDate("January 1, 2021 00:00:00").getTime();
-  // const now = newDate().getTime();
-  // const distance =startCountdown - now;
+  const Completionist = () => <span>You have an event today!</span>;
 
-  // const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  // const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 *60*60));
-  // const minutes = Math.floor(distance % (1000 * 60 * 60 ))/(1000 * 60);
+  const countDownDate = ()=>  new Date (props.setEvent.eventDate).getTime();
+  // console.log(countDownDate)  
+  function eventDateInterval(){
+    var date = countDownDate - Date.now ();
 
-
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  }
   // Renderer callback with condition
   const renderer = ({ days, hours, minutes, completed }) => {
     if (completed) {
@@ -40,8 +92,10 @@ function EventCountdown(props) {
       <h1> Your next event starts in ... </h1>
       <Container>
         <Countdown
-
-          date={Date.now() + 1000 * 60 * 60 * 24}
+            date={countDownDate - Date.now ()}
+         
+      
+          // renderer = {renderer}
 
 
         />,
